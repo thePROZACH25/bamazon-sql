b4be185452da02730795ca20b86339f9de3b525d
Zach S.L <zach.s.ledford@gmail.com>
2020-04-24 20:39:13 -0400

    functioning bamazonCustomer.js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,44 +1,102 @@
 var mysql = require("mysql");
 var inquire = require("inquirer");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "Playstation1!",
   database: "bamazon",
 });
 
 connection.connect(function (err) {
   if (err) throw err;
-  startSear();
+  display();
 });
 
-function startSear() {
+var display = function () {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    console.log("----------------------------");
+    console.log("     Welcome to Bamzon      ");
+    console.log("----------------------------");
+    console.log("");
+    console.log("Find Your Product Below");
+    console.log("");
+    var table = new Table({
+      head: ["item_id", "product_name", "price"],
+      colWidths: [9, 25, 9],
+      colAligns: ["center", "center", "right"],
+      style: {
+        head: ["aqua"],
+        compact: true,
+      },
+    });
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].price]);
+    }
+    console.log(table.toString());
+    console.log("");
+  });
+  shop();
+};
+
+var shop = function () {
   inquire
     .prompt({
-      name: "menu",
-      type: "list",
-      message: "Welcome to Bamazon!",
-      choices: ["Search Product", "Exit"],
+      name: "productToBuy",
+      type: "input",
+      message: "Enter a item_id of the item you wish to buy!",
     })
-    .then(function (answer) {
-      switch (answer.menu) {
-        case "Search Product":
-          productSear();
-          break;
+    .then(function (answerFirst) {
+      var select = answerFirst.productToBuy;
 
-        case "Exit":
-          connection.end();
-          break;
-      }
+      connection.query(
+        "SELECT * FROM products WHERE item_id=?",
+        select,
+        function (err, res) {
+          if (err) throw err;
+          if (res.length === 0) {
+            console.log(
+              "That product doesn't exist, enter a item_id from the list above"
+            );
+            shop();
+          } else {
+            inquire.prompt({
+              name: "quantity",
+              type: "input",
+              message: "How many would you like to purchase?"
+            }).then(function(answerSec){
+              var quantity = answerSec.quantity;
+
+              if(quantity> res[0].stock_quantity){
+                console.log("Our Apologies! Insufficient quantity. We only have " + res[0].stock_quantity + "items of the product.")
+                shop();
+              }else{
+                var endTotal = quantity * res[0].price
+                console.log("");
+                console.log(res[0].product_name + " purchased");
+                console.log(quantity + " qty @ $" + res[0].price);
+                console.log("Total: $" + endTotal);
+                console.log("");
+
+                var newQuantity = res[0].stock_quantity - quantity;
+                connection.query(
+                  "UPDATE products SET stock_quantity = " + newQuantity + " WHERE item_id = " + res[0].item_id, function(err, resUpdate){
+                    if (err) throw err;
+                    console.log('');
+                    console.log('Your order has been processed!');
+                    console.log("****Thank you for shopping with us!****");
+                    console.log('');
+                    connection.end();
+                  }
+                )
+              }
+            });
+          }
+        }
+      );
     });
-}
+};
 
-function productSear() {}
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -5,10 +5,13 @@ USE bamazon;
 
 CREATE TABLE products (
   item_id INT NOT NULL PRIMARY KEY,
-  product_name VARCHAR(100) NULL,
-  department_name VARCHAR(100) NULL,
-  stock_quantity  INT NULL,
-  price DECIMAL(10,4) NULL 
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  stock_quantity INT NULL,
+  price DECIMAL(10,4) NOT NULL 
 );
 
 SELECT * FROM products;
+
+-- INSERT INTO products(product_name, department_name, stock_quantity, price)
+-- VALUE
